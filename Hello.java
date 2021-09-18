9e3349fce883bed822b1a5fc14378730fb380c5e
Jashu Kumar <jashukumar348@gmail.com>
2021-09-18 17:07:48 +0530

    Java Collection Framework List
     Collection Framework

--- Hello.java
+++ Hello.java
@@ -4,17 +4,17 @@ import java.util.*;
 		 int[] a={10};
 	public static void main(String[] args)
            {
-/*ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+ArrayList<String> list=new ArrayList<String>();//Creating arraylist
 list.add("Ravi");//Adding object in arraylist
 list.add("Vijay");
 list.add("Ravi");
 list.add("Ajay");
 
-//Traversing list through Iterator
-//Iterator itr=list.iterator();
-//while(itr.hasNext()){
-//System.out.println(itr.next());
-//}
+Traversing list through Iterator
+Iterator itr=list.iterator();
+while(itr.hasNext()){
+System.out.println(itr.next());
+}
 
 	System.out.println("List is\n"+list);
 	list.remove("Vijay");
@@ -25,15 +25,15 @@ list.add("Ajay");
 					   System.out.println("List removed");
 					   list.removeAll(list);
 					   System.out.println(list);*/
-					  //while(false)
-					  //char ch='\ubeef';
-						//   System.out.println(ch);
+					  while(false)
+					  char ch='\ubeef';
+						   System.out.println(ch);
 
-						//int[] a=new int[2];
+						int[] a=new int[2];
 
-						//System.out.println(a[0]);
-						//String[] s={"Array","ABout"};
-						//Object[] a=s;
+						System.out.println(a[0]);
+						String[] s={"Array","ABout"};
+						Object[] a=s;
 
                   System.out.println(a[0]);
                   Hello h=new Hello();
