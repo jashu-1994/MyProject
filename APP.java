258859a8fd1a6198e1e70946b3df132152f6e8a6
Jashu Kumar <jashukumar348@gmail.com>
2021-09-18 16:54:43 +0530

    Maven Demo
    Maven Demo  from zero

---
+++ APP.java
@@ -0,0 +1,12 @@
+package com.codebind;
+
+public class APP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public String Sample()
+	{return "sample";}
+
+}
