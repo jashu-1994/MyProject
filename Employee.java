1bb4ec9a0c3e2bb6936cdba65d954291955d1dba
Jashu Kumar <jashukumar348@gmail.com>
2021-09-27 23:17:52 +0530

    registration-jsp-servlet(MVC)
    contoller
    dao model view

--- Employee.java
+++ Employee.java
@@ -1,95 +1,48 @@
-package com.softech.entity;
+package net.javaguide.registration.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "EMPLOYEE")
 public class Employee {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "EMPLOYEE_ID")
-	private int ID;
-
-	@Column(name = "NAME")
-	private String name;
-
-	@Column(name = "GENDER")
-	private String gander;
-
-	@Column(name = "ADDRESS")
-	private String address;
-
-	@Column(name = "CITY")
-	private String city;
-
-	@Column(name = "SALARY")
-	private int salary;
-
-	
-
-	public int getID() {
-		return ID;
+	private String firstName;
+    public String getFirstName() {
+		return firstName;
 	}
-
-	public Employee() {
-		super();
-		// TODO Auto-generated constructor stub
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-
-	public void setID(int iD) {
-		ID = iD;
+	public String getLastName() {
+		return lastName;
 	}
-
-	public String getName() {
-		return name;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-
-	public void setName(String name) {
-		this.name = name;
+	public String getUsername() {
+		return username;
 	}
-
-	public String getGander() {
-		return gander;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-
-	public void setGander(String gander) {
-		this.gander = gander;
+	public String getPassword() {
+		return password;
 	}
-
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-	public String getCity() {
-		return city;
+	public String getContact() {
+		return contact;
 	}
-
-	public void setCity(String city) {
-		this.city = city;
+	public void setContact(String contact) {
+		this.contact = contact;
 	}
-
-	public int getSalary() {
-		return salary;
-	}
-
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [ID=" + ID + ", name=" + name + ", gander=" + gander + ", address=" + address + ", city="
-				+ city + ", salary=" + salary + "]";
-	}
+	private String lastName;
+    private String username;
+    private String password;
+    private String address;
+    private String contact;
+    
 
 }
